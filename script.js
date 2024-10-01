10dcb74d79852ec2c39ee2ffc513ed23ab0a876c
Akshita-1706 <160586394+Akshita-1706@users.noreply.github.com>
2024-10-01 21:16:35 +0530

    Add files via upload
    Completed this project under Verve Bridge's Internship
    Thank you Verve Bridge for this opportunity

---
+++ script.js
@@ -0,0 +1,8 @@
+// import "./style.css";
+
+// import products from "./product.json";
+
+// Shop now button 
+function redirectToShop() {
+    window.location.href = 'Shop.html'; // Change 'Shop.html' to the desired URL
+}
